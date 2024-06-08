22bd2363b5d9acfcf6a9e892afc392fd600f1460
葉明淳 <148878625+Emyeee@users.noreply.github.com>
2024-06-08 15:55:50 +0800

    Add files via upload
    因為index.css裡面有建一些components(container等等), 所以.js檔案需要做一點變動。主要是在最下面return的部分。
    主要的功能是沒有動到的。

---
+++ pages/Login/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css';
+import loginImage from '../../assets/login.png'; // Adjust the path as necessary
+
+const Login = ({ changePage }) => {
+
+    function identity(s) {
+        changePage(s)
+    }
+
+    return (
+        <div className="flex-container">
+            <img src={loginImage} alt="Login" className= ".login-image" />
+            <div className="login-container">
+                <div className="login-title">登入</div>
+                <button className="btn0" onClick={() => { identity('TherapistLogin') }}>治療師</button><br />
+                <button className="btn0" onClick={() => { identity('ParentsLogin') }}>家長</button><br />
+                <button className="btn0" onClick={() => { identity('NewAccount') }}>創建新帳號</button>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
